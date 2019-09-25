c0323d148156cb253b507f05da73e49d817b2172
Dina Hrist <hdina@inbox.ru>
2019-09-25 22:47:16 +0700

    Done dropdown menu and add slider main

---
+++ src/blocks/components/slider-main/SliderMain.js
@@ -0,0 +1,24 @@
+import SliderBase from "../../../js/classes/SliderBase";
+
+class SliderMain extends SliderBase {
+    bindOptions(...options) {
+        super.bindOptions(...options, {
+            nextArrow: "<div class=\"slider-button next\"></div>",
+            prevArrow: "<div class=\"slider-button prev\"></div>",
+        });
+    }
+
+    init() {
+        super.init();
+        this.initWrapDots();
+    }
+
+    initWrapDots() {
+        let dots = this.slider.find('.slick-dots');
+        if (dots) {
+            dots.wrap('<div class="slick-dots-wrap">');
+        }
+    }
+}
+
+export {SliderMain};
---
+++ src/js/import/components.js
@@ -0,0 +1,6 @@
+import $ from "jquery";
+import {SliderMain} from "%components%/slider-main/SliderMain";
+
+$(document).ready(function () {
+    new SliderMain('.js-slider-main');
+});
